088b1c625461d77db68867bf252f64540cc8f541
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-05-19 09:46:33 +0530

    Update Navbar.js

--- 18th may assignment/components/Navbar.js
+++ 18th may assignment/components/Navbar.js
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
         <ul>
-            <li><a class="active" href="#home">Home</a></li>
+            <li><a className="active" href="#home">Home</a></li>
             <li><a href="#news">News</a></li>
             <li><a href="#contact">Contact</a></li>
             <li className="about"><a href="#about">About</a></li>
